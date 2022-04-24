af009c1d5d1f9c3743858097eb3863e2cf36050b
Sergey Vertegel <s.vertegel@gmail.com>
2022-04-24 14:10:24 +0300

    add random num generator and guesses watcher

--- chapter_1/get_number.rb
+++ chapter_1/get_number.rb
@@ -2,10 +2,19 @@
 # Written by: vrtx
 
 puts "Welcome to 'Get My Number!'"
+
+# get username and output greeting
 print "What's your name? "
-
 input = gets
-
 name = input.chomp
-
 puts "Welcome #{name}!"
+
+# random nuber save
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
+
+# watching for number of guesses left
+num_guesses = 0
+
+puts "You've got #{10 - num_guesses} guesses left."
