2a483fe0f1e60ad2385cc2f26da1eebb4095526b
Sergey Vertegel <s.vertegel@gmail.com>
2022-04-24 23:13:52 +0300

    add defaul parameter to use_headlights

--- chapter_1/vehicle_methods.rb
+++ chapter_1/vehicle_methods.rb
@@ -8,7 +8,7 @@ def sound_horn
   puts "Beep beep!"
 end
 
-def use_headlights(brightness)
+def use_headlights(brightness="low-beam")
   puts "Turning on #{brightness} headlights"
   puts "Watch out for deer!"
 end
