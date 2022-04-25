f9cfd5ddb4277208a01ea126a27ace825798aee3
Sergey Vertegel <s.vertegel@gmail.com>
2022-04-25 21:18:32 +0300

    add instance variables for classes

--- chapter_1/oop_animals.rb
+++ chapter_1/oop_animals.rb
@@ -1,42 +1,86 @@
 class Bird
-  def talk(name)
-    puts "#{name} says Chirp! Chirp!"
+  def make_up_name
+    @name = "Popka"
   end
-  def move(name, destination)
-    puts "#{name} flies to the #{destination}."
+
+  def talk
+    puts "#{@name} says Chirp! Chirp!"
+  end
+
+  def move(destination)
+    puts "#{@name} flies to the #{destination}."
+  end
+
+  def make_up_age
+    @age = 2
+  end
+
+  def report_age
+    puts "#{@name} is #{@age} years old."
   end
 end
 
 class Dog
-  def talk(name)
-    puts "#{name} says Bark!"
+  def make_up_name
+    @name = "Lucy"
   end
-  def move(name, destination)
-    puts "#{name} runs to the #{destination}."
+
+  def talk
+    puts "#{@name} says Bark!"
+  end
+
+  def move(destination)
+    puts "#{@name} runs to the #{destination}."
+  end
+
+  def make_up_age
+    @age = 5
+  end
+
+  def report_age
+    puts "#{@name} is #{@age} years old."
   end
 end
 
 class Cat
-  def talk(name)
-    puts "#{name} says Meow!"
+  def make_up_name
+    @name = "Fluffy"
   end
-  def move(name, destination)
-    puts "#{name} runs to the #{destination}."
+
+  def talk
+    puts "#{@name} says Meow!"
+  end
+
+  def move(destination)
+    puts "#{@name} runs to the #{destination}."
+  end
+
+  def make_up_age
+    @age = 3
+  end
+
+  def report_age
+    puts "#{@name} is #{@age} years old."
   end
 end
 
 bird = Bird.new
+bird.make_up_name
+bird.talk
+bird.move("window")
+bird.make_up_age
+bird.report_age
+
 dog = Dog.new
+dog.make_up_name
+dog.talk
+dog.move("fence")
+dog.make_up_age
+dog.report_age
+
 cat = Cat.new
-
-dog_name = "Lucy"
-dog.talk(dog_name)
-dog.move(dog_name, "fence")
-
-cat_name = "Fluffy"
-cat.talk(cat_name)
-cat.move(cat_name, "litter box")
-
-bird_name = "Popka"
-bird.talk(bird_name)
-bird.move(bird_name, "window")
+cat.make_up_name
+cat.talk
+cat.move("litter box")
+cat.make_up_age
+cat.report_age
